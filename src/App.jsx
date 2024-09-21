d06255ee3b2ef123a628fa79a2e5ef893114cace
Mohamed Essam <109111496+m-essam-s@users.noreply.github.com>
2024-09-21 15:55:40 +0300

    Accepting props in class component

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,16 @@ import './App.css'
 
 import { Component } from 'react'
 import PropTypes from 'prop-types'
-
+import Header from './components/Header'
+import Greeting from './components/Greeting'
 class App extends Component {
   render() {
     return (
-      <h1>
-        {this.props.type} Component
-      </h1>
+      <div className='app-container'>
+        <h1>{this.props.type} Component</h1>
+        <Header username="John Doe" />   
+        <Greeting />
+      </div>
     )
   }
 }
